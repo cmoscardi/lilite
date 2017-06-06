c5d9ec9b093cd37f480b26a38d9cd2c1bce8ed80
Christian Moscardi <moscardi79@gmail.com>
2017-06-05 22:17:18 -0400

    oops forgot to pass the parameters

--- test_packages.py
+++ test_packages.py
@@ -10,7 +10,7 @@ import seed_packages as sp
 
 def main():
     db_session, Package, InstallMethod = sp.init_db()
-    sp.main()
+    sp.main(db_session, Package, InstallMethod)
     packages = [p['name'] for p in sp.load_json()]
     app, client = create_app_client()
 
