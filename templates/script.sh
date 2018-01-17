866a90ae6be1db9a10bd2a8bac8cf490a80b4005
Christian Moscardi <moscardi79@gmail.com>
2018-01-17 11:35:13 -0500

    Revert "potential chrome fix"
    This reverts commit 34ab9c3f0023dde18ad610a6c8842920b8877270.

--- templates/script.sh
+++ templates/script.sh
@@ -5,9 +5,6 @@ apt-get -y update
 # standard packages
 apt-get -y install {% for package in standard %}{{ package }} {% endfor %}
 
-VERSION=`lsb_release -a | grep Description | cut -d ':' -f 2 | xargs | cut -d ' ' -f 2`
-echo "Version is $VERSION"
-
 # weird packages
 {% for pre, package, post in weird %}
 {% if pre %}{{pre}}{% endif %}
